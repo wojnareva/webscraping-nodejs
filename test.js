532bf6bf752dec1cf9762e0e0f067e696f0d3ec6
wojnareva <126489853+wojnareva@users.noreply.github.com>
2025-05-25 00:09:14 +0200

    Add basic web scraper using Axios and Cheerio to extract 5-letter words

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
 // const cheerio = require("cheerio");
 import * as cheerio from 'cheerio';
-import * as axios from 'axios';
+import axios from 'axios';
+
+axios.get('https://www.slovensky.eu/slova-na-5-pismen/')
+    .then((response) => {
+        const $ = cheerio.load(response.data);
+        $('#searchresults li a').each(function(){
+            console.log($(this).text());
+        });
+    })
+    .catch((error) => {
+        console.log('error');
+    })
+    .then(() => {
+
+    });
