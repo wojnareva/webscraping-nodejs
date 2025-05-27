d1cbfe8ab5d1266cd3c0831c27590b073b19c99c
wojnareva <126489853+wojnareva@users.noreply.github.com>
2025-05-27 13:05:30 +0200

    Add image URL extraction and resolution upgrade for book covers. Fix image URL by replacing 100x100 with 440x440 and converting format to .jpg

--- main.js
+++ main.js
@@ -23,27 +23,35 @@ axios.get(url)  // Send HTTP GET request to the target page
             const ratingText = item.find('.ProductItemRating_rating__CdLUd').text().trim();   
             // Extract rating text and convert comma to dot:         
             const rating = ratingText ? parseFloat(ratingText.replace(',', '.')) : null;
+
+            const imageSource = item.find('img').attr('data-src') ||
+                    item.find('source[type="image/jpeg"]').attr('srcset') ||
+                    '';
+            const imageUrl = imageSource.replace('100x100', '440x440')
+            .replace('.webp', '.jpg');;
+
+            
             const linkPath = item.find('.ProductItemLink_root__BYR_c').attr('href');
             const link = linkPath?.startsWith('http') ? linkPath : 'https://knihobot.cz' + linkPath;
 
             // Only push the book to results if it has a title (basic filter)
             if (title) {
                 // console.log({ author, title, state, price, link });
-                results.push({ author, title, state, priceText, price, ratingText, rating, link });
+                results.push({ author, title, state, priceText, price, ratingText, rating, imageSource, image: imageUrl, link });
             }
         });
 
         // Output total number of books found
         console.log(`Total books scraped: ${results.length}`);
 
-        // Save the results to a JSON file
-        // JSON.stringify(...):
-        // - results: the array of book objects
-        // - null: no custom replacer function (not needed here)
-        // - 2: pretty-print with 2-space indentation for readability
-        // writeFileSync(...):
-        // - 'books.json': target filename
-        // - 'utf-8': ensures the file is written using UTF-8 encoding (important for accented characters)
+        // Save the results to a JSON file:
+            // JSON.stringify(...):
+            // - results: the array of book objects
+            // - null: no custom replacer function (not needed here)
+            // - 2: pretty-print with 2-space indentation for readability
+            // writeFileSync(...):
+            // - 'books.json': target filename
+            // - 'utf-8': ensures the file is written using UTF-8 encoding (important for accented characters)
         writeFileSync('books.json', JSON.stringify(results, null, 2), 'utf-8');
     })
     .catch((error) => { // Handling errors with Axios
